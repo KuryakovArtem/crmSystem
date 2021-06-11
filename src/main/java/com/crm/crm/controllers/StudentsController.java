1aa980129e70203f85d581e4fa6fbe19492f3278
KuryakovArtem <kurik01tagil@gmail.com>
2021-06-12 01:33:59 +0300

    added group field

--- src/main/java/com/crm/crm/controllers/StudentsController.java
+++ src/main/java/com/crm/crm/controllers/StudentsController.java
@@ -1,15 +1,15 @@
 package com.crm.crm.controllers;
 
+import com.crm.crm.models.Groups;
 import com.crm.crm.models.Students;
+import com.crm.crm.repos.GroupsRepository;
 import com.crm.crm.repos.StudentsRepository;
 import com.crm.crm.repos.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.ArrayList;
 import java.util.Optional;
 
@@ -17,6 +17,9 @@ import java.util.Optional;
 public class StudentsController {
     @Autowired
     private StudentsRepository studentsRepository;
+
+    @Autowired
+    private GroupsRepository groupsRepository;
 
     @GetMapping("/students")
     public String studentsMain(Model model){
@@ -26,7 +29,7 @@ public class StudentsController {
     }
 
     @GetMapping("/students/add")
-    public String studentsAdd(Model model){
+    public String studentsAdd(){
         return "students-add";
     }
 
@@ -35,9 +38,10 @@ public class StudentsController {
                                  @RequestParam String second_name,
                                  @RequestParam String patronymic,
                                  @RequestParam Integer balance,
+                                 @RequestParam String group,
                                  Model model){
 
-        Students students = new Students(first_name, second_name, patronymic, balance);
+        Students students = new Students(first_name, second_name, patronymic, balance, group);
         studentsRepository.save(students);   //this is where students added to students table
         return  "redirect:/students";
     }
@@ -60,6 +64,8 @@ public class StudentsController {
         if(!studentsRepository.existsById(id))        {
             return "redirect:/students";
         }
+        Iterable<Groups> groups = groupsRepository.findAll();
+        model.addAttribute("groupsList", groups);
         Optional<Students> students = studentsRepository.findById(id);
         ArrayList<Students> res = new ArrayList<>();
         students.ifPresent(res::add);
@@ -73,12 +79,14 @@ public class StudentsController {
                                      @RequestParam String second_name,
                                      @RequestParam String patronymic,
                                      @RequestParam Integer balance,
+                                     @RequestParam String my_group,
                                      Model model){
        Students student = studentsRepository.findById(id).orElseThrow();
        student.setFirst_name(first_name);
        student.setSecond_name(second_name);
        student.setPatronymic(patronymic);
        student.setBalance(balance);
+       student.setMyGroup(my_group);
        studentsRepository.save(student);
 
        return "redirect:/students";
@@ -86,8 +94,7 @@ public class StudentsController {
 
     @PostMapping("/students/{id}/delete")
     public String studentPostRemove(@PathVariable(value = "id") long id,
-                                     Model model)
-    {
+                                     Model model){
         Students student = studentsRepository.findById(id).orElseThrow();
         studentsRepository.delete(student);
 
