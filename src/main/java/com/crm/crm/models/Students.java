65f85c83b1774a2b527e6381bdd9e77105d3eda4
KuryakovArtem <kurik01tagil@gmail.com>
2021-06-12 01:32:26 +0300

    deleted @ElementCollection...

--- src/main/java/com/crm/crm/models/Students.java
+++ src/main/java/com/crm/crm/models/Students.java
@@ -1,12 +1,9 @@
 package com.crm.crm.models;
 
-import com.crm.crm.entity.UserRole;
-
 import javax.persistence.*;
 
 @Entity
 public class Students {
-
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
@@ -14,8 +11,24 @@ public class Students {
     private String second_name;
     private String patronymic;
     private Integer balance;
-    @ElementCollection(targetClass = UserRole.class, fetch = FetchType.EAGER)
-    @CollectionTable(name = "students", joinColumns = @JoinColumn(name = "id"))
+    private String myGroup;
+
+
+
+//    @ElementCollection(targetClass = UserRole.class, fetch = FetchType.EAGER)
+//    @CollectionTable(name = "students", joinColumns = @JoinColumn(name = "id"))
+
+//    @ManyToOne(fetch = FetchType.EAGER)
+//    @JoinColumn(name = "students_id") // похоже создается поле в таблице группы
+//    private Groups groups;
+
+//    public Groups getGroups() {
+//        return groups;
+//    }
+//
+//    public void setGroups(Groups groups) {
+//        this.groups = groups;
+//    }
 
     public Long getId() {
         return id;
@@ -57,16 +70,23 @@ public class Students {
         this.balance = balance;
     }
 
+    public String getMyGroup() {
+        return myGroup;
+    }
 
+    public void setMyGroup(String myGroup) {
+        this.myGroup = myGroup;
+    }
     public Students()
     {
 
     }
 
-    public Students(String first_name, String second_name, String patronymic, Integer balance) {
+    public Students(String first_name, String second_name, String patronymic, Integer balance, String myGroup) {
         this.first_name = first_name;
         this.second_name = second_name;
         this.patronymic = patronymic;
         this.balance = balance;
+        this.myGroup = myGroup;
     }
 }
