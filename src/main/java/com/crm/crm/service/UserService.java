19832c9e57000c463f655b0b09343adccc7ddd4f
KuryakovArtem <kurik01tagil@gmail.com>
2021-06-13 19:16:08 +0300

    added UserService.java

--- src/main/java/com/crm/crm/service/UserService.java
+++ src/main/java/com/crm/crm/service/UserService.java
@@ -1,23 +1,58 @@
-//package com.crm.crm.service;
-//
-//import com.crm.crm.repos.UserRepository;
-//import lombok.AllArgsConstructor;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.core.userdetails.UsernameNotFoundException;
-//import org.springframework.stereotype.Service;
-//
-//@Service
-//@AllArgsConstructor
-//public class UserService implements UserDetailsService {
-//    private final static String USER_NOT_FOUND_MESSAGE = "There is no such user with %s email";
-//
-//    private final UserRepository userRepository;
-//    @Override
-//    public UserDetails loadUserByUsername(String email)
-//            throws UsernameNotFoundException {
-//        return userRepository.findByEmail(email)
-//                .orElseThrow(() ->
-//                        new UsernameNotFoundException(String.format(USER_NOT_FOUND_MESSAGE,email)));
+package com.crm.crm.service;
+
+import com.crm.crm.entity.User;
+import com.crm.crm.repos.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService{
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    public boolean isUserPresent(String email) {
+        User user=userRepository.findByEmail(email);
+        if(user!=null)
+            return true;
+
+        return false;
+    }
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public User findByUsername(String username){
+        return userRepository.findByUsername(username);
+    }
+
+    public boolean activateUser(String code) {
+        User user = userRepository.findByActivationCode(code);
+        if(user == null){
+            return false;
+        }
+        user.setActivationCode(null);
+        userRepository.save(user);
+        return true;
+    }
+//    public List<User> findByName(String name) {
+//        return  userRepository.findByNameLike("%"+name+"%");
 //    }
-//}
+
+//    public boolean addUser(User user){
+//        User userFromDb = userRepository.findByUsername(user.getUsername());
+//        if (userFromDb!=null){
+//            return false;
+//        }
+//
+//
+//    }
+
+}
