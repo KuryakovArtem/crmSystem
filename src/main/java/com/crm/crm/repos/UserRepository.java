69b245f0fa745c00fbd512bf44a443454970e62a
KuryakovArtem <kurik01tagil@gmail.com>
2021-06-13 19:10:49 +0300

    added methods in UserRepository.java

--- src/main/java/com/crm/crm/repos/UserRepository.java
+++ src/main/java/com/crm/crm/repos/UserRepository.java
@@ -5,10 +5,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 @Transactional(readOnly = true)
 public interface UserRepository extends JpaRepository<User, Long>{
     User findByUsername(String username);
+    User findByEmail(String email);
+
+    User findByActivationCode(String code);
+//    List<User> findByNameLike(String name);
 }
