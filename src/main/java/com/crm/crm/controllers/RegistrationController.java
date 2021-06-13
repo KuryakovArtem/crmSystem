f2e4f1ab98179a526fefdebd530cdc9877f9768d
KuryakovArtem <kurik01tagil@gmail.com>
2021-06-13 19:15:13 +0300

    added methods activate(email)

--- src/main/java/com/crm/crm/controllers/RegistrationController.java
+++ src/main/java/com/crm/crm/controllers/RegistrationController.java
@@ -7,17 +7,19 @@ import com.crm.crm.models.Students;
 import com.crm.crm.repos.InstructorsRepository;
 import com.crm.crm.repos.StudentsRepository;
 import com.crm.crm.repos.UserRepository;
+import com.crm.crm.service.MailSender;
+import com.crm.crm.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Controller
 public class RegistrationController {
@@ -27,6 +29,12 @@ public class RegistrationController {
     private StudentsRepository studentsRepository;
     @Autowired
     InstructorsRepository instructorsRepository;
+    @Autowired
+    UserService userService;
+
+
+    @Autowired
+    MailSender mailSender;
 
     @GetMapping("/registration")
     public String registration(Model model){
@@ -37,6 +45,19 @@ public class RegistrationController {
         return "registration";
     }
 
+    @GetMapping("/activate/{code}")
+    public String activate(Model model, @PathVariable String code){
+        boolean isActivated = userService.activateUser(code);
+
+        if(isActivated){
+            model.addAttribute("message","User activated");
+        }
+        else{
+            model.addAttribute("message", "activationCodeIsNotFound");
+        }
+
+        return "login";
+    }
     @PostMapping("/registration")
     public String addUser(User user, Map<String, Object> model, @RequestParam String userRole){
         User userFromDB = userRepository.findByUsername(user.getUsername());
@@ -62,14 +83,25 @@ public class RegistrationController {
 
             instructorsRepository.save(instructors);
             user.setRoles(Collections.singleton(UserRole.TEACHER));
-        }
+        }else
+
 
 
         user.setActive(true);
 
         //user.setRoles(Collections.singleton(UserRole.STUDENT)); //This is a role placeholder
+        user.setActivationCode(UUID.randomUUID().toString());
         userRepository.save(user);
-
+        if(!(user.getEmail().isEmpty())){
+            String message = String.format(
+                    "Hello, %s!\n" +
+                            " Welcome to Spring CRM Autoschool!" +
+                            " please, visit next link: http://localhost:8080/activate/%s",
+                    user.getUsername(),
+                    user.getActivationCode()
+            );
+            mailSender.send(user.getEmail(), "Activation code", message);
+        }
         return "redirect:/login";
     }
 }
